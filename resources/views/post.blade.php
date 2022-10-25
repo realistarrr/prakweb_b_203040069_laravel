8c7de06aec66f0bf8da8ff974c2adc8af549fb94
realistarrr <religirealista2411@gmail.com>
2022-10-25 15:55:42 +0700

    menyelesaikan video #8

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,6 +2,8 @@
 
 @section('container')
     <h1 class="mb-5">{{  $post->title  }}</h1>
+
+    <p>By. Realista in <a href="/categories/{{ $post->category->slug }}"> {{ $post->category->name   }}</a></p>
     
     {!! $post->body !!}
 
