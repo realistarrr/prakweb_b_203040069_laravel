43b7d7157ce068377489840eb5f0f356968f0d31
realistarrr <religirealista2411@gmail.com>
2022-10-31 19:58:29 +0700

    Menyelesaikan Video #12 - UI Redesign

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -4,18 +4,23 @@
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
+
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" href="/">Home</a>
+          <a class="nav-link {{ ($title === "home") ? 'active': '' }}" href="/">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/about">About</a>
+          <a class="nav-link {{ ($title === "about") ? 'active': '' }}" href="/about">About</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="/blog">Blog</a>
-            </li>
+            <a class="nav-link{{ ($title === "posts") ? 'active': '' }}" href="/blog">Blog</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link{{ ($title === "categories") ? 'active': '' }}" href="/categories">Categories</a>
+        </li>
         </ul>
-        </div>
+    </div>
+
     </div>
     </nav>
