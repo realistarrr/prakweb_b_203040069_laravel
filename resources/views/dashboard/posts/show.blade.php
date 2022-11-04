a4dcddd7d8b30d4b4aa4c41ba03962a067982a2b
realistarrr <religirealista2411@gmail.com>
2022-11-05 06:17:16 +0700

    Menyelesaikan Video #17 Dashboard UI

---
+++ resources/views/dashboard/posts/show.blade.php
@@ -0,0 +1,24 @@
+@extends('dashboard.layouts.main')
+
+@section('container')
+<div class="container">
+    <div class="row my-3">
+        <div class="col-lg-8">
+        <h1 class="mb-3">{{  $post->title  }}</h1>
+
+        <a href="/dashboard/posts" class="btn btn-success"><span data-feather="arrow-left" class="align-text-bottom"></span> Back to my post</a>
+        <a href="" class="btn btn-warning"><span data-feather="edit" class="align-text-bottom"></span> Edit</a>
+        <a href="" class="btn btn-danger"><span data-feather="x-circle" class="align-text-bottom"></span> Delete</a>
+
+        <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" alt="{{ $post->category->name }}" class="img-fluid">
+
+        <article class="my-3 fs-5">
+        {!! $post->body !!}
+        </article>
+
+        <a href="/blog" class="d-block mt-3">Back to Posts</a>
+        </div>
+    </div>
+</div>
+
+@endsection
